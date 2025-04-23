b93ef6caddd6c67b296499a7a94c0fe4ca1a93f0
Rakesh709 <kumar.rakesh1997@outlook.com>
2025-04-23 22:38:00 +0530

    auth controller register

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,7 +1,28 @@
 import bcrypt from "bcryptjs"
+import {db} from "../libs/db.js"
+import { json } from "express";
 
+export const register = async ( req, res) =>{
+    const {name, email, password} = req.body;
 
-export const register = async ( req, res) =>{}
+    try {
+        const existingUser = await db.user.findUnique({
+            where:{
+                email
+            }
+        })
+        if(!existingUser){
+            return res.status(400, json({
+               error: "User alredy exists" 
+            }))
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+    } catch (error) {
+        
+    }
+
+}
 
 export const login = async (req, res) =>{}
 
