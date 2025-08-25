6d5fa9ea041ca18e4444b4d29f58e517552977d1
Rakesh709 <kumar.rakesh1997@outlook.com>
2025-08-25 22:13:04 +0530

    auth controllers

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,28 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import authRoutes from "./routes/auth.routes.js";
+
+
 
 const app = express();
 dotenv.config();
+app.use(express.json());
 
+app.use(cookieParser());
 const port = 8000 || process.env.PORT
+
+
+
+
+
+app.get("/",(req,res)=>{
+    res.send("Hello guys welcome to leetProblem");
+    
+})
+
+app.use("/api/v1/auth",authRoutes)
+
 
 app.listen(port,()=>{
     console.log("Server is running",port
