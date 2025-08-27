aecd5cf733d06f8b08f7acb8d046253c5d2ccbfa
Rakesh709 <kumar.rakesh1997@outlook.com>
2025-08-27 15:11:28 +0530

    problems auth and middleware

--- backend/prisma/migrations/20250827085941_models/migration.sql
+++ backend/prisma/migrations/20250827085941_models/migration.sql
@@ -1,5 +1,22 @@
 -- CreateEnum
+CREATE TYPE "public"."UserRole" AS ENUM ('ADMIN', 'USER');
+
+-- CreateEnum
 CREATE TYPE "public"."Difficulty" AS ENUM ('EASY', 'MEDIUM', 'HARD');
+
+-- CreateTable
+CREATE TABLE "public"."User" (
+    "id" TEXT NOT NULL,
+    "name" TEXT,
+    "email" TEXT NOT NULL,
+    "image" TEXT,
+    "role" "public"."UserRole" NOT NULL DEFAULT 'USER',
+    "password" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
 
 -- CreateTable
 CREATE TABLE "public"."Problem" (
@@ -21,6 +38,9 @@ CREATE TABLE "public"."Problem" (
 
     CONSTRAINT "Problem_pkey" PRIMARY KEY ("id")
 );
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "public"."User"("email");
 
 -- AddForeignKey
 ALTER TABLE "public"."Problem" ADD CONSTRAINT "Problem_userId_fkey" FOREIGN KEY ("userId") REFERENCES "public"."User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
