57bddb605d587243503629ae4ec29fcf84f4fac7
Rakesh709 <kumar.rakesh1997@outlook.com>
2025-08-30 21:54:32 +0530

    code execution continue

--- backend/src/controllers/executeCode.controller.js
+++ backend/src/controllers/executeCode.controller.js
@@ -1,5 +1,5 @@
-import { pollBatchResults, submitBatch } from "../libs/judge0.lib.js";
-
+import { getLanguageName, pollBatchResults, submitBatch } from "../libs/judge0.lib.js";
+import { db } from "../libs/db.js";
 
 export const executeCode = async (req,res) =>{
     try {
@@ -38,8 +38,87 @@ export const executeCode = async (req,res) =>{
         const results  = await pollBatchResults(tokens);
 
 
-        console.log("Results-------------------------");
+        console.log("Execution Results -------------------------");
         console.log(results);
+
+        //analyze test cases results
+        //standard o/p sai expected o/p sai compare kr rhe hai
+        
+        let allPassed = true;
+        const detailedResults = results.map((result,i)=>{
+            const stdout = result.stdout?.trim();
+            const expected_output = expected_outputs[i]?.trim();
+            const passed = stdout === expected_output;
+
+            if(!passed) allPassed = false;
+
+            return {
+                testCase:i+1,
+                passed,
+                stdout,
+                expected:expected_output,
+                stderr:result.stderr || null,
+                compile_output:result.compile_output || null,
+                status:result.status.description,
+                memory:result.memory ? `${result.memory} KB`: undefined,
+                time: result.time? `${result.time} s` : undefined
+            }
+
+            // console.log(`TestCases #${i+1}`);
+            // console.log(`Input for testcases #${i+1} : ${stdin[i]}`);
+            // console.log(`Expected ooytput for the test cases#${i+1} : ${expected_output}`);
+            // console.log(`Actual output for testcases #${i+1} : ${stdout}`);
+            
+            // console.log(`Matched test cases #${i+1} : ${passed}`);
+        
+            
+        })
+
+        console.log(detailedResults);
+        
+        //store submisiion summary
+
+        const submission = await db.submission.create({
+            data:{
+                userId,
+                problemId,
+                sourceCode: source_code,
+                language:getLanguageName(language_id),
+                stdin:stdin.join("\n"),
+                stdout:JSON.stringify(detailedResults.map((r) => e.stdout)),
+                stderr:detailedResults.some((r)=>r.stderr)
+                ? JSON.stringify(detailedResults.map((r)=>r.stderr))
+                :null,
+                compileOutput:detailedResults.some((r)=>r.compile_output)
+                ? JSON.stringify(detailedResults.map((r)=> r.compile_output))
+                :null,
+                status:allPassed ? "Accepted": "Wrong Answer",
+                memory:detailedResults.some((r)=> r.memory)
+                ? JSON.stringify(detailedResults.map((r)=> r.memory))
+                :null,
+                time:detailedResults.some((r)=>r.time)
+                ? JSON.stringify(detailedResults.map((r)=> r.time))
+                :null,
+
+            },
+        });
+
+        // if all passed = true mark problem as solved for the current user
+        if(allPassed){
+            await db.problemSolved.upsert({
+                where:{
+                    userId_problemId:{
+                        userId,problemId
+                    }
+                },
+                update:{},
+                create:{
+                    userId,problemId
+                }
+            })
+        }
+
+        
 
         res.status(200).json({
             message:"Code executed !"
