32164ab83357f290d788661859cdee032c1953cc
Rakesh709 <kumar.rakesh1997@outlook.com>
2025-09-02 15:29:22 +0530

    toast implemeted

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,31 @@
 
-import React from "react"
+import React, { useEffect } from "react"
 import { Routes,Route,Navigate } from "react-router-dom"
+import {Toaster} from "react-hot-toast"
+
 import HomePage from "./page/HomePage.jsx"
 import LoginPage from "./page/LoginPage.jsx"
 import SignUpPage from "./page/SignUpPage.jsx"
+import { useAuthStore } from "./store/useAuthStore.js"
+import { Loader } from "lucide-react";
 
 const App =( ) =>{
+  const {authUser,checkAuth,isCheckingAuth} = useAuthStore()
 
-  let authUser = null;
+  useEffect(()=>{
+    checkAuth()
+  },[checkAuth])
+
+  if(isCheckingAuth && !authUser){
+    return(
+      <div className="flex items-center justify-center h-screen">
+        <Loader className="size-10 animate-spin" />
+      </div>
+    )
+  }
   return(
     <div className="flex flex-col items-center justify-start">
+    <Toaster/>
         <Routes>
 
           <Route
