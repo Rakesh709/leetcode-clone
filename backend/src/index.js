241f45bcd6bb8495e0132a898a3eb12cafb2c2d0
Rakesh709 <kumar.rakesh1997@outlook.com>
2025-09-03 23:08:06 +0530

    fixing cors issue

--- backend/src/index.js
+++ backend/src/index.js
@@ -14,7 +14,7 @@ dotenv.config();
 app.use(express.json());
 app.use(
     cors({
-        origin:"http://localhost:5174",
+        origin:"http://localhost:5173",
         credentials:true
     })
 )
